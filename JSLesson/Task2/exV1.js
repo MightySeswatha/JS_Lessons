02fbebb56def32ecb78ee1406ebc04ebd252ff3d
MightySeswatha <teszerorobot@gmail.com>
2022-06-29 20:16:43 +0300

    My Commit1

--- JSLesson/Task2/exV1.js
+++ JSLesson/Task2/exV1.js
@@ -1,5 +1,16 @@
 "use strict";
 let y,x,k,z;
-const e = ;
-const pi = ;
-y = Math.pow(,Math.pow(e,k*x));
+const e = 2.71828182846;
+const pi = 3.14159265359;
+k = Number(prompt("Enter k"));
+z = Number(prompt("Enter z"));
+if(k < 1){
+document.write("<p> k<1 </p>");
+x = k*Math.pow(z,3);
+}
+else if(k >= 1){
+document.write("<p> k>=1 </p>");	
+x = z*(z+1);
+} 
+y = Math.pow(Math.log(1+x*x)+Math.cos(x+1),Math.pow(e,k*x));
+document.write("<p>"+y+"</p>");

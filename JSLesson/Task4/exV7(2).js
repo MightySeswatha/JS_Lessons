20064197391791116b6eed0f0fcb4690e1499976
MightySeswatha <teszerorobot@gmail.com>
2022-07-09 22:25:12 +0300

    NC

---
+++ JSLesson/Task4/exV7(2).js
@@ -0,0 +1,22 @@
+"use strict";
+let arr1 = [];
+let arr2 = [];
+let N = Number(prompt("Enter N:"));
+if(isNaN(N) == true){
+  System("Pause");
+}
+for(let i = 0; i < N; i++){
+  arr1[i] = prompt("Enter element("+i+")");
+}
+for(let i = 0; i < N; i++){
+  arr2[i] = prompt("Enter element("+i+")");
+}
+console.log(arr1);
+console.log(arr2);
+//Функция сортировки
+function sort(arr1,arr2){
+
+}
+
+
+document.write(arr);
